bef72afe7d4740db9f740f8f7d0baa3f241b95cc
root <root@ubuntu.localdomain>
2018-09-12 11:03:20 +0200

    minor changes

--- setup.py
+++ setup.py
@@ -6,18 +6,19 @@ from __future__ import print_function
 from distutils.core import setup
 
 setup(
-    name='JackIt',
+    name='JackItNG',
     version='0.1.0',
-    author='infamy and phikshun',
+    author='Curious Master',
     packages=['jackit', 'jackit.lib', 'jackit.plugins'],
     scripts=['bin/jackit','bin/jacklst'],
-    url='https://github.com/insecurityofthings/jackit',
+    url='https://github.com/curious.master/JackItNG',
     license='BSD',
     description='Exploit framework for MouseJack vulnerability.',
     install_requires=[
         "click==5.1",
         "pyusb==1.0.0",
         "tabulate==0.7.5",
+        "sqlite3==2.6.0",
         "six==1.10.0"
     ],
 )
