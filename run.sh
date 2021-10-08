5d7113428379ef0ad2dac49ea8b1c691ff2bb6aa
Curious Master <curious.master@gmail.com>
2021-10-08 06:48:24 +0200

    added run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!  /usr/bin/sh
+
+bin/jackit \
+	--script "/home/steben/Projects/JackItNG/payloads/Windows/chrome.txt" \
+	--address A6:24:11:44:07 \
+	--vendor logitech \
+	--interval 2 \
+	--layout sv
