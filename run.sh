3c0456a17b8b198e21000f7e0cd2fdb62fb7c376
root <root@msetestkali01>
2026-02-18 12:14:00 +0100

    Major TUI update - textual

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!  /usr/bin/sh
 
 PAYLOAD=pay.pay
-ADDRESS=93:99:93:F5:0D
+ADDRESS=93:99:93:F5:18
 VENDOR=logitech
 INTERVAL=5
+DURATION=5
 KBD=sv
 
 
 bin/jackit \
   	--reset \
 	--script ${PAYLOAD} \
-	--address ${ADDRESS} \
-	--vendor ${VENDOR} \
 	--interval ${INTERVAL} \
-	--layout ${KBD}
+	--duration ${DURATION} \
+	--layout ${KBD} \
+	#--address ${ADDRESS} \
+	#--vendor ${VENDOR} \
